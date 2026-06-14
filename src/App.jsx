be644fc333ea97dd6e50e883031121a004e78d9d
amirulaliff2006 <amirulaliffkk2016@gmail.com>
2026-06-14 12:07:06 +0800

    update latest mcfest features

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import * as XLSX from "xlsx"
 import jsPDF from "jspdf"
-import html2canvas from "html2canvas"
+
 import { useEffect, useState } from "react"
 import { QRCodeCanvas } from "qrcode.react"
 import paymentQR from "./assets/paymentQR.jpeg"
@@ -350,7 +350,7 @@ e.target.reset()
     setWalkinName("")
     setWalkinPax(1)
   }
-  function downloadTicketPDF(ticket) {
+ function downloadTicketPDF(ticket) {
   const qrCanvas = document.querySelector("#ticket-pdf canvas")
 
   if (!qrCanvas) {
@@ -359,33 +359,32 @@ e.target.reset()
   }
 
   const qrImage = qrCanvas.toDataURL("image/png")
-
   const pdf = new jsPDF("p", "mm", "a4")
 
   pdf.setFillColor(0, 0, 0)
   pdf.rect(0, 0, 210, 297, "F")
 
   pdf.setTextColor(220, 38, 38)
-  pdf.setFontSize(26)
+  pdf.setFontSize(28)
   pdf.text("MCFEST TICKET", 105, 25, { align: "center" })
 
-  pdf.addImage(qrImage, "PNG", 65, 35, 80, 80)
+  pdf.addImage(qrImage, "PNG", 65, 40, 80, 80)
 
   pdf.setTextColor(255, 255, 255)
   pdf.setFontSize(14)
 
-  pdf.text(`Ticket ID: ${ticket.ticketId}`, 20, 135)
-  pdf.text(`Group: ${ticket.groupName}`, 20, 145)
-  pdf.text(`Payment: ${ticket.paymentStatus}`, 20, 155)
-  pdf.text(`Status: ${ticket.status}`, 20, 165)
-  pdf.text(`Total: 5 Pax`, 20, 175)
-  pdf.text(`Hari: ${ticket.day}`, 20, 185)
-  pdf.text(`Tarikh: ${ticket.date}`, 20, 195)
-  pdf.text(`Session: ${ticket.session}`, 20, 205)
+  pdf.text(`Ticket ID: ${ticket.ticketId}`, 20, 140)
+  pdf.text(`Group: ${ticket.groupName}`, 20, 152)
+  pdf.text(`Payment: ${ticket.paymentStatus}`, 20, 164)
+  pdf.text(`Status: ${ticket.status}`, 20, 176)
+  pdf.text(`Total: 5 Pax`, 20, 188)
+  pdf.text(`Hari: ${ticket.day}`, 20, 200)
+  pdf.text(`Tarikh: ${ticket.date}`, 20, 212)
+  pdf.text(`Session: ${ticket.session}`, 20, 224)
 
   pdf.setTextColor(160, 160, 160)
   pdf.setFontSize(11)
-  pdf.text("Sila tunjuk QR ini semasa hadir ke event.", 105, 230, {
+  pdf.text("Sila tunjuk QR ini semasa hadir ke event.", 105, 250, {
     align: "center",
   })
 
